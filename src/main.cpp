07e7e85c420497d8a1ed04ef0a2ba27018774a6c
Alex <opti.jawsome@gmail.com>
2019-11-09 17:49:27 -0600

    Cy Springs Post-Comp Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,3 @@
-// To complete the VEXcode V5 Text project upgrade process, please follow the
-// steps below.
-// 
-// 1. You can use the Robot Configuration window to recreate your V5 devices
-//   - including any motors, sensors, 3-wire devices, and controllers.
-// 
-// 2. All previous code located in main.cpp has now been commented out. You
-//   will need to migrate this code to the new "int main" structure created
-//   below and keep in mind any new device names you may have set from the
-//   Robot Configuration window. 
-// 
-// If you would like to go back to your original project, a complete backup
-// of your original (pre-upgraded) project was created in a backup folder
-// inside of this project's folder.
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// ---- END VEXCODE CONFIGURED DEVICES ----
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
@@ -35,13 +13,13 @@ competition Competition;
 
 // define your global instances of motors and other devices here
 controller Controller;
-motor frontLeftMotor  (PORT10,  gearSetting::ratio18_1, false);
-motor backLeftMotor   (PORT1,   gearSetting::ratio18_1, false);
-motor frontRightMotor (PORT20,  gearSetting::ratio18_1, false);
-motor backRightMotor  (PORT11,  gearSetting::ratio18_1, false);
-motor ctaMotor        (PORT16,  gearSetting::ratio18_1, false);
-motor intakeMotorOne  (PORT21,  gearSetting::ratio18_1, false);
-motor intakeMotorTwo  (PORT22,  gearSetting::ratio18_1, false);
+motor frontLeftMotor   (PORT12,  gearSetting::ratio18_1, false);
+motor backLeftMotor    (PORT11,   gearSetting::ratio18_1, false);
+motor frontRightMotor  (PORT10,  gearSetting::ratio18_1, false);
+motor backRightMotor   (PORT9,   gearSetting::ratio18_1, false);
+motor ctaMotor         (PORT16,  gearSetting::ratio18_1, false);
+motor intakeMotorLeft  (PORT13,  gearSetting::ratio18_1, false);
+motor intakeMotorRight (PORT8,   gearSetting::ratio18_1, false);
 
 
 
@@ -93,45 +71,63 @@ void usercontrol( void ) {
   // User control code here, inside the loop
   double straight;
   double rotate;
-  bool isPullingBack = false;
+  Brain.Screen.clearScreen();
+  Brain.Screen.print("Actual Comp code is running");
+  // bool isPullingBack = false;
   while (1) {
 
     // Drive Code (Split Arcade) //
     straight = Controller.Axis3.position();
     rotate = Controller.Axis1.position();
 
-    frontLeftMotor  .spin(directionType::fwd, (straight - rotate), velocityUnits::pct);
-    backLeftMotor   .spin(directionType::fwd, (straight - rotate), velocityUnits::pct);
-    frontRightMotor .spin(directionType::fwd, (straight + rotate), velocityUnits::pct);
-    backRightMotor  .spin(directionType::fwd, (straight + rotate), velocityUnits::pct);
+    frontLeftMotor  .spin(directionType::fwd, (straight + rotate), velocityUnits::pct);
+    backLeftMotor   .spin(directionType::fwd, (straight + rotate), velocityUnits::pct);
+    frontRightMotor .spin(directionType::fwd, (straight - rotate), velocityUnits::pct);
+    backRightMotor  .spin(directionType::fwd, (straight - rotate), velocityUnits::pct);
 
 
     // Cube Tray Angler //
-    if (Controller.ButtonR1.pressing()){ // Top Right Bumper (R1): Push Cube Tray Forward
-      isPullingBack = false;
-      while (1) {
-        ctaMotor.spin(directionType::fwd, 50, velocityUnits::pct);
-        if (ctaMotor.rotation(rotationUnits::deg) > 90){
-          ctaMotor.stop(coast);
-          break;
-        }
-      }
-    } else if (Controller.ButtonR2.pressing()) { // Bottom Right Bumper (R2): Pull Cube Tray Back
-      isPullingBack = true;
+    if (Controller.ButtonR1.pressing()) {
+      ctaMotor.rotateTo(0, rotationUnits::deg, 50, velocityUnits::pct, false);
+    }
+    if (Controller.ButtonR2.pressing()) {
+      ctaMotor.rotateTo(185, rotationUnits::deg, 50, velocityUnits::pct, false);
     }
 
-    if (isPullingBack) { // Code to Pull Cube Tray Back
-      if (ctaMotor.rotation(rotationUnits::deg) < 45) {
-        ctaMotor.spin(directionType::rev, 50, velocityUnits::pct);
-      } else {
-        ctaMotor.stop();
-      }
+    if (Controller.ButtonL1.pressing()) {
+      intakeMotorLeft.spin(directionType::fwd, 100, velocityUnits::pct);
+    } else {
+      intakeMotorLeft.stop();
+    }
+    if (Controller.ButtonL1.pressing()) {
+      intakeMotorRight.spin(directionType::fwd, 100, velocityUnits::pct);
+    } else {
+      intakeMotorRight.stop();
     }
 
-    if (Controller.ButtonL1.pressing()){
-      intakeMotorOne.spin(directionType::fwd, 100, velocityUnits::pct);
-      intakeMotorTwo.spin(directionType::rev, 100, velocityUnits::pct);
-    }
+    // if (Controller.ButtonR1.pressing()){ // Top Right Bumper (R1): Push Cube Tray Forward
+    //   isPullingBack = false;
+    //   while (1) {
+    //     ctaMotor.spin(directionType::fwd, 50, velocityUnits::pct);
+    //     if (ctaMotor.rotation(rotationUnits::deg) > 90){
+    //       ctaMotor.stop(coast);
+    //       break;
+    //     }
+    //   }
+    // } else if (Controller.ButtonR2.pressing()) { // Bottom Right Bumper (R2): Pull Cube Tray Back
+    //   isPullingBack = true;
+    // }
+    // if (isPullingBack) { // Code to Pull Cube Tray Back
+    //   if (ctaMotor.rotation(rotationUnits::deg) < 45) {
+    //     ctaMotor.spin(directionType::rev, 50, velocityUnits::pct);
+    //   } else {
+    //     ctaMotor.stop();
+    //   }
+    // }
+    // if (Controller.ButtonL1.pressing()){
+    //   intakeMotorLeft.spin(directionType::fwd, 100, velocityUnits::pct);
+    //   intakeMotorRight.spin(directionType::rev, 100, velocityUnits::pct);
+    // }
   }
 }
 
@@ -139,16 +135,16 @@ void usercontrol( void ) {
 //
 // Main will set up the competition functions and callbacks.
 //
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  Competition.autonomous( autonomous );
-  Competition.drivercontrol( usercontrol );
+int main(){
+    //Set up callbacks for autonomous and driver control periods.
+    Competition.autonomous( autonomous );
+    Competition.drivercontrol( usercontrol );
     
-  //Run the pre-autonomous function.                         
-  while(1) {
-    vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
-  } 
+    //Run the pre-autonomous function. 
+    pre_auton();
+       
+    //Prevent main from exiting with an infinite loop.                        
+    while(1) {
+      vex::task::sleep(20);//Sleep the task for a short amount of time to prevent wasted resources.
+    }    
 }
-
-        
