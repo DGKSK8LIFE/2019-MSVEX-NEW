b1e902333b101d645bb10022bb9849b207b87357
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2019-11-21 16:11:32 -0600

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ int main(){
     //Set up callbacks for autonomous and driver control periods.
     Competition.autonomous( autonomous );
     Competition.drivercontrol( usercontrol );
-       
+
     //Prevent main from exiting with an infinite loop.                        
     while(1) {
       vex::task::sleep(20);//Sleep the task for a short amount of time to prevent wasted resources.
